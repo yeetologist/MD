e600ef913bfddde9df6cf87844eb09a158d2a530
Mohammad Asyrof Jundullah Rifai <101625556+MohammadAsyrof@users.noreply.github.com>
2023-12-12 01:46:48 +0700

    push SplashScreenActivity and AndroidManifest

--- app/src/main/java/com/dicoding/parentpal/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/dicoding/parentpal/ui/SplashScreenActivity.kt
@@ -1,4 +1,19 @@
 package com.dicoding.parentpal.ui
 
-class SplashScreenActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.parentpal.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+            finish()
+        },1000)
+    }
 }
