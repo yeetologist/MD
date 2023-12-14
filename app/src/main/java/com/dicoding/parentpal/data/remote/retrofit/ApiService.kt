d98ca4979c0be3dd985b8d025c59c0f154b9669e
yeetologist <makanmakan9c@gmail.com>
2023-12-14 11:43:38 +0700

    Change news fetch time behavior

--- app/src/main/java/com/dicoding/parentpal/data/remote/retrofit/ApiService.kt
+++ app/src/main/java/com/dicoding/parentpal/data/remote/retrofit/ApiService.kt
@@ -1,6 +1,7 @@
 package com.dicoding.parentpal.data.remote.retrofit
 
 import com.dicoding.parentpal.data.remote.response.NewsResponse
+import com.dicoding.parentpal.util.getOneMonthBeforeCurrentTime
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -9,7 +10,7 @@ interface ApiService {
     @GET("everything")
     suspend fun getListNews(
         @Query("q") query: String = "Health",
-        @Query("from") fromDate: String = "2023-12-1",
+        @Query("from") fromDate: String = getOneMonthBeforeCurrentTime(),
         @Query("sortBy") sortBy: String = "popularity",
         @Query("apiKey") apiKey: String = "c3775a471c674664b618c6a70ae128e8",
         @Query("page") page: Int,
--- app/src/main/java/com/dicoding/parentpal/util/Utils.kt
+++ app/src/main/java/com/dicoding/parentpal/util/Utils.kt
@@ -2,10 +2,18 @@ package com.dicoding.parentpal.util
 
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 fun ImageView.loadImage(url: String) {
     Glide.with(this.context)
         .load(url)
         .centerCrop()
         .into(this)
+}
+
+fun getOneMonthBeforeCurrentTime(): String {
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+    val oneMonthBefore = LocalDate.now().minusMonths(1)
+    return oneMonthBefore.format(formatter)
 }
