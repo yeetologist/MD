9e16c771efb738dadf01dbea266122b8a61613c0
yeetologist <makanmakan9c@gmail.com>
2023-12-14 11:10:22 +0700

    Add image to news list

---
+++ app/src/main/java/com/dicoding/parentpal/util/Utils.kt
@@ -0,0 +1,11 @@
+package com.dicoding.parentpal.util
+
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+fun ImageView.loadImage(url: String) {
+    Glide.with(this.context)
+        .load(url)
+        .centerCrop()
+        .into(this)
+}
