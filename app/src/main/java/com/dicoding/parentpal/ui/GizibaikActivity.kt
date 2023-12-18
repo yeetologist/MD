a757eced7571e43b723472d4ab917be5123f80c2
Mohammad Asyrof Jundullah Rifai <101625556+MohammadAsyrof@users.noreply.github.com>
2023-12-19 02:37:24 +0700

    push GizibaikActivity OverweightActivity StuntingActivity UnderweigethActivity

---
+++ app/src/main/java/com/dicoding/parentpal/ui/GizibaikActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.parentpal.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.parentpal.R
+
+class GizibaikActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gizibaik)
+    }
+}
---
+++ app/src/main/java/com/dicoding/parentpal/ui/UnderweigethActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.parentpal.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.parentpal.R
+
+class UnderweigethActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_underweigeth)
+    }
+}
