6820f5b27b6ed761cc48bd9028b1d992b4ffdaef
yeetologist <makanmakan9c@gmail.com>
2023-12-20 19:23:23 +0700

    Add navigation to calc button

--- app/src/main/java/com/dicoding/parentpal/ui/HomeFragment.kt
+++ app/src/main/java/com/dicoding/parentpal/ui/HomeFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.dicoding.parentpal.R
 import com.dicoding.parentpal.databinding.FragmentHomeBinding
 import com.dicoding.parentpal.util.PreferenceManager
+
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
@@ -25,6 +28,10 @@ class HomeFragment : Fragment() {
         preferenceManager = PreferenceManager(requireContext())
 
         binding.tvUsername.text = preferenceManager.getPreferences()?.name ?: ""
+
+        binding.cvMain.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_calculatorFragment)
+        }
 
     }
 }
